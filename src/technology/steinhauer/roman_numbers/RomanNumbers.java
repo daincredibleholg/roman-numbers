c54562ef85ff50231ff53963847fa5487148460c
Holger Steinhauer <holger.steinhauer@prodyna.com>
2014-07-10 22:48:55 +0200

    Added additional numbers and calculation for roman number representation. The lower bound is checked now.

--- src/technology/steinhauer/roman_numbers/RomanNumbers.java
+++ src/technology/steinhauer/roman_numbers/RomanNumbers.java
@@ -11,15 +11,35 @@ public class RomanNumbers {
 
     static {
         latinToRomanNumber.put(1000, "M");
+        latinToRomanNumber.put(900, "CM");
         latinToRomanNumber.put(500, "D");
+        latinToRomanNumber.put(400, "CD");
         latinToRomanNumber.put(100, "C");
+        latinToRomanNumber.put(90, "XC");
         latinToRomanNumber.put(50, "L");
+        latinToRomanNumber.put(40, "XL");
         latinToRomanNumber.put(10, "X");
+        latinToRomanNumber.put(9, "IX");
         latinToRomanNumber.put(5, "V");
+        latinToRomanNumber.put(4, "IV");
         latinToRomanNumber.put(1, "I");
     }
 
     public static String convertFromInt(final int number) {
-        return latinToRomanNumber.get(number);
+        if (number == 0) {
+            throw new IllegalArgumentException("0 isn't available in roman numbers.");
+        }
+        int rest = number;
+        String result = "";
+        for (Map.Entry curMapping : latinToRomanNumber.entrySet()) {
+            int latinNumber = (Integer) curMapping.getKey();
+            String romanNumber = (String) curMapping.getValue();
+            while (rest >= latinNumber) {
+                result += romanNumber;
+                rest -= latinNumber;
+            }
+
+        }
+        return result;
     }
 }
--- src/technology/steinhauer/roman_numbers/RomanNumbersTest.java
+++ src/technology/steinhauer/roman_numbers/RomanNumbersTest.java
@@ -19,10 +19,82 @@ public class RomanNumbersTest {
 
     @Test
     public void anotherSimpleConversion() {
-        String expectedResult = "X";
         int latinNmber = 10;
+        String expectedResult = "X";
 
         String actualResult = RomanNumbers.convertFromInt(latinNmber);
         assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void  checkCombination () {
+        int latinNumber = 2;
+        String expectedResult = "II";
+
+        String actualResult = RomanNumbers.convertFromInt(latinNumber);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void checkFour () {
+        int latinNumber = 4;
+        String expectedResult = "IV";
+
+        String actualResult = RomanNumbers.convertFromInt(latinNumber);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void checkNine () {
+        int latinNumber = 9;
+        String expectedResult = "IX";
+
+        String actualResult = RomanNumbers.convertFromInt(latinNumber);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void checkFourty () {
+        int latinNumber = 40;
+        String expectedResult = "XL";
+
+        String actualResult = RomanNumbers.convertFromInt(latinNumber);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void checkNinty() {
+        int latinNumber = 90;
+        String expectedResult = "XC";
+
+        String actualResult = RomanNumbers.convertFromInt(latinNumber);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void checkFourHundred () {
+        int latinNumber = 400;
+        String expectedResult = "CD";
+
+        String actualResult = RomanNumbers.convertFromInt(latinNumber);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void checkNineHunded () {
+        int latinNumber = 900;
+        String expectedResult = "CM";
+
+        String actualResult = RomanNumbers.convertFromInt(latinNumber);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void checkZero () {
+        int latinNumber = 0;
+        String actualResult = RomanNumbers.convertFromInt(latinNumber);
+
+        fail("0 isn't part of roman numbers");
+
     }
 }
